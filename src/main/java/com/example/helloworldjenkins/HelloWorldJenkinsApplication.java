a7ad87208a1c20cf5d331fe64a814830dcac7605
Lalit <lalit_bhamare@yahoo.com>
2022-12-13 16:09:25 +0530

    First Commit

---
+++ src/main/java/com/example/helloworldjenkins/HelloWorldJenkinsApplication.java
@@ -0,0 +1,19 @@
+package com.example.helloworldjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldJenkinsApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(HelloWorldJenkinsApplication.class, args);
+        InApplication();
+    }
+    public static  void InApplication()
+    {
+        System.out.println("Inside Spring Boot Application");
+    }
+
+}
